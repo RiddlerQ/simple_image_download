36f449f2c66afaaf8be605f01e9be8d37a1de432
Koubae <58447627+Koubae@users.noreply.github.com>
2020-12-05 07:05:13 +0100

    Bug fixes, New Layout and Functionalities
    Signed-off-by: Koubae <58447627+Koubae@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,34 +1,155 @@
-Google image downloader
+# Google image downloader
 =======================
 Python script that lets you search for urls of images from google images using your tags and/or download them automatically onto you computer
 
-Documentation
+
+## Documentation
 -------------
 
-1. Installation
+### 1. Installation
 
-   - Download simple-image-download.py from my [github](https://github.com/RiddlerQ/simple_image_download) and use it in your project
-   - Use command **pip install simple_image_download**
-   
-2. Methods
+Download simple-image-download.py from my github and use it in your project
+Use command pip install simple_image_download
 
-   In my script simple_image_dowload is a class so the correct way to use it in you project would be to import simple_image_download
-   from simple_image_download like this **from simple_image_download import simple_image_download as simp** and then create new variable
-   for example **response = simp.simple_image_download**.
-   
-   Next you can use response to activate methods:
-   
-   - response().download(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}) -> downloads images to new directory
-     - keywords - for instance -> 'bear, Mario'
-	 - limit - for instance -> 5
-	 
-   - response().urls(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}) -> returns urls of google images
-     - keywords - for instance -> 'bear'
-	 - limit - for instance -> 5
+#### Note on Windows for [python-magic 0.4.18](https://pypi.org/project/python-magic/) which is used for this Project
+You'll need DLLs for libmagic [Here](https://pypi.python.org/pypi/python-magic-bin/0.4.14)  the includes binaries to PyPI.
+##### **Or Just run:**
+``` pip install python-magic-bin==0.4.14```
+##### Window 64-bit!!!
+If you are using a 64-bit build of python, you'll need 64-bit libmagic binaries which can be found here: https://github.com/pidydx/libmagicwin64. 
+Drop the dlls in C:\Windows\System32 and python magic will import correctly.
+-- Newer version can be found here: https://github.com/nscaife/file-windows.
 
-3. Example
+
+---------------------------------------------------------------------------------------------------------------
+
+### 2. Quickstart
+
+The main class is  Download import it in your project as so:
+```
+from simple_image_download import Downloader simp 
+```
+Then create a new class instance
+```
+response = simp.simple_image_download()
+```
+Next you can use response to activate methods:
+```
+response.download(keywords, [limit])
+```
+ 
+### Downloads Images
+```
+from simple_image_download import Downloader simp 
+ 
+response = simp.simple_image_download()
+response.download(‘bear supermario’, limit=5)
+ 
+```
+#### Get Only the Pictures URL and Store them in the Class cache:
+
+```
+from simple_image_download import Downloader simp 
+response = simp.simple_image_download()
+response.search_urls(‘bear supermario’, limit=15)
+ 
+for url in response.cache:
+    print(url)
+ 
+```
+ 
+---------------------------------------------------------------------------------------------------------------
+ 
+### 3. API
+ 
+#### *class* simple_image_download.Downloader(extensions=None)
+ 
+**Parameters**:
+* extension -- Type of extension allowed to be downloaded, if left None defaults are:
+ {‘.jpg’, ‘.png’, ‘.ico’, ‘.gif’, ‘.jpeg’}
+ 
+-------------------------------------------------------------
+
+#### Functions
+ 
+#### search_urls(keywords, limit, verbose=False, cache=True, timer=None)
+ 
+This functions returns and Caches URLs of Pictures based on:
+ 
+1. Keywords for the search
+2. File Extensions based on the class instance you define
+3. How many picture per keyword you need with the limit parameter
+ 
+* verbose => Output the links in the terminal in real time
+
+* cache => if set to False, the URLs won’t be stored in the class instance, default is True
+
+* timer => Default is set to 100_000 Looks up, defines the number of WebPages's 'chunks' will search. In the function scan_webpage a 100_000 lookkup means that it will loop up to 100_000 before stop.
+
+Usefule in case of a picture that is not been found, so won't allow to loop indefinitely.
+ 
+#### download(keywords, limit, verbose=False, cache=True, download_cache=False, timer=None)
+
+This functions downloads pictures into defined class instance’s directory:
+ 
+1. Keywords for the search
+2. File Extensions based on the class instance you define
+3. How many picture per keyword you need with the limit parameter
+4. The directory is named after the Keyword.
+5. Pictures have a unique ID, so that multiple downloads can persome
+ 
+* verbose => Output the links in the terminal in real time
+
+* cache => if set to False, the URLs won’t be stored in the class instance, default is True
+
+* download_cache => allows to download all the URLs stored in the Downloader's instance Cache. Remember to clear the cache afterwards with Downloader.flush_cache
+
+* timer => Default is set to 100_000 Looks up, defines the number of WebPages's 'chunks' will search. In the function scan_webpage a 100_000 lookkup means that it will loop up to 100_000 before stop.
+
+Usefule in case of a picture that is not been found, so won't allow to loop indefinitely.
+ 
+#### flash_cache():
+ 
+Clears the class instance’s cache which is stored in instance.cached_urls
+
+-------------------------------------------------------------
+ 
+#### Properties
+ 
+#### directory
+ 
+The directory where the Picture are saved, default is in ‘simple_images/’.
+You can set the default directory like this:
+```
+my_downloader = simp.simple_image_download()
+my_downloader.directory = ‘my_dir/bla/’
+
+```
+
+#### get_dirs
+
+
+Set of all sub Direcotories where the picture where saved .
+ 
+#### cached_urls
+
+ 
+All of the cached urls perfomed with the seach of the function Downloader.get_urls()
+
+Is a Dictionary with this schema:
+
+{'file_name': [Dir_path, URL_content]}
+ 
+User Downloader.flash_cache() to clear it or run mydownloader.download(download_cache=True)
+to download the whole content.
+
+---------------------------------------------------------------------------------------------------------------
+
+
+### 3. Example
+
 	
-	Example are on my [github](https://github.com/RiddlerQ/simple_image_download) in sub-respository named example
+	Example are on my [github](https://github.com/Koubae/simple_image_download/tree/master/Example) 
 	
 End
 ---
--- simple_image_download/simple_image_download.py
+++ simple_image_download/simple_image_download.py
@@ -1,174 +1,197 @@
 import os
-import time
 import urllib
 import requests
 import magic
 import progressbar
 from urllib.parse import quote
+import random
+from requests.exceptions import  ReadTimeout
+import functools
+import time
 
-class simple_image_download:
-    def __init__(self):
+
+################
+# ---> CONSTANTS
+################
+
+BASE_URL = 'https://www.google.com/search?q='
+GOOGLE_PICTURE_ID = '''&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ'''
+HEADERS = {
+    'User-Agent':
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+}
+SCANNER_COUNTER = None
+
+
+def generate_search_url(keywords):
+    keywords_to_search = [str(item).strip() for item in keywords.split(',')][0].split()
+    keywords_count = len(keywords_to_search)
+    return keywords_to_search, keywords_count
+
+
+def generate_urls(search):
+    """Generates a URLS in the correct format that brings to Google Image seearch page"""
+    return [(BASE_URL+quote(word)+GOOGLE_PICTURE_ID) for word in search]
+
+
+def check_webpage(url):
+    checked_url = None
+    try:
+        request = requests.get(url, allow_redirects=True, timeout=10)
+        if 'html' not in str(request.content):
+            checked_url = request
+    except ReadTimeout as err:
+        print(err)
         pass
+    return checked_url
 
-    def urls(self, keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
-        keyword_to_search = [str(item).strip() for item in keywords.split(',')]
-        i = 0
-        links = []
 
-        things = len(keyword_to_search) * limit
+def scan_webpage(webpage, extensions, timer):
+    """Scans for pictures to download based on the keywords"""
+    global SCANNER_COUNTER
+    scanner = webpage.find
+    found = False
+    counter = 0
+    while counter < timer:
+        new_line = scanner('"https://', SCANNER_COUNTER + 1)  # How Many New lines
+        SCANNER_COUNTER = scanner('"', new_line + 1)  # Ends of line
+        buffor = scanner('\\', new_line + 1, SCANNER_COUNTER)
+        if buffor != -1:
+            object_raw = webpage[new_line + 1:buffor]
+        else:
+            object_raw = webpage[new_line + 1:SCANNER_COUNTER]
+        if any(extension in object_raw for extension in extensions):
+            found = True
+            break
+        counter += 1
+    if found:
+        object_ready = check_webpage(object_raw)
+        return object_ready
 
-        bar = progressbar.ProgressBar(maxval=things, \
+
+class Downloader:
+    """
+        Main Downloader
+        ::param extension:iterable of Files extensions
+    """
+    def __init__(self, extensions=None):
+        if extensions:
+            self._extensions = set(*[extensions])
+        else:
+            self._extensions = {'.jpg', '.png', '.ico', '.gif', '.jpeg'}
+        self._directory = "simple_images/"
+        self.get_dirs = set()
+        self._cached_urls = {}
+
+    @property
+    def directory(self):
+        return self._directory
+
+    @directory.setter
+    def directory(self, value):
+        self._directory = value
+
+    @property
+    def cached_urls(self):
+        return self._cached_urls
+
+    @property
+    def extensions(self):
+        return self._extensions
+
+    @extensions.setter
+    def extensions(self, value):
+        self._extensions = set([value])
+
+
+    def get_urls(self):
+        return [self._cached_urls[url][1].url
+                for url in self._cached_urls]
+
+    def _download_page(self, url):
+        req = urllib.request.Request(url, headers=HEADERS)
+        resp = urllib.request.urlopen(req)
+        resp_data = str(resp.read())
+        return resp_data
+
+    def search_urls(self, keywords, limit=1, verbose=False, cache=True, timer=None):
+        cache_out = {}
+        search, count = generate_search_url(keywords)
+        urls_ = generate_urls(search)
+        timer = timer if timer else 1000
+        max_progressbar = count * (list(range(limit+1))[-1]+1)
+        bar = progressbar.ProgressBar(maxval=max_progressbar,
                                       widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
-
-        while i < len(keyword_to_search):
-            url = 'https://www.google.com/search?q=' + quote(
-                keyword_to_search[i].encode(
-                    'utf-8')) + '&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ'
-            raw_html = self._download_page(url)
-
-            end_object = -1;
-            google_image_seen = False;
-            j = 0
-
-            while j < limit:
-                while (True):
-                    try:
-                        new_line = raw_html.find('"https://', end_object + 1)
-                        end_object = raw_html.find('"', new_line + 1)
-
-                        buffor = raw_html.find('\\', new_line + 1, end_object)
-                        if buffor != -1:
-                            object_raw = (raw_html[new_line + 1:buffor])
-                        else:
-                            object_raw = (raw_html[new_line + 1:end_object])
-
-                        if any(extension in object_raw for extension in extensions):
-                            break
-
-                    except Exception as e:
-                        break
-
-
-                try:
-                    r = requests.get(object_raw, allow_redirects=True, timeout=1)
-                    if('html' not in str(r.content)):
-                        mime = magic.Magic(mime=True)
-                        file_type = mime.from_buffer(r.content)
-                        file_extension = f'.{file_type.split("/")[1]}'
-                        if file_extension == '.png' and not google_image_seen:
-                            google_image_seen = True
-                            raise ValueError();
-                        links.append(object_raw)
-                        bar.update(bar.currval + 1)
-                    else:
-                        j -= 1
-                except Exception as e:
-                    j -= 1
-                j += 1
-
-            i += 1
-
-        bar.finish()
-        return(links)
-
-
-    def download(self, keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
-        keyword_to_search = [str(item).strip() for item in keywords.split(',')]
-        main_directory = "simple_images/"
         i = 0
-
-        things = len(keyword_to_search) * limit
-
-        bar = progressbar.ProgressBar(maxval=things, \
-                                      widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-
-        bar.start()
-
-        while i < len(keyword_to_search):
-            self._create_directories(main_directory, keyword_to_search[i])
-            url = 'https://www.google.com/search?q=' + quote(
-                keyword_to_search[i].encode('utf-8')) + '&biw=1536&bih=674&tbm=isch&sxsrf=ACYBGNSXXpS6YmAKUiLKKBs6xWb4uUY5gA:1581168823770&source=lnms&sa=X&ved=0ahUKEwioj8jwiMLnAhW9AhAIHbXTBMMQ_AUI3QUoAQ'
-            raw_html = self._download_page(url)
-
-            end_object = -1;
-            google_image_seen = False;
-            j = 0
-            while j < limit:
-                while (True):
-                    try:
-                        new_line = raw_html.find('"https://', end_object + 1)
-                        end_object = raw_html.find('"', new_line + 1)
-
-                        buffor = raw_html.find('\\', new_line + 1, end_object)
-                        if buffor != -1:
-                            object_raw = (raw_html[new_line+1:buffor])
-                        else:
-                            object_raw = (raw_html[new_line+1:end_object])
-
-                        if any(extension in object_raw for extension in extensions):
-                            break
-
-                    except Exception as e:
-                        break
-                path = main_directory + keyword_to_search[i].replace(" ", "_")
-
-                try:
-                    r = requests.get(object_raw, allow_redirects=True, timeout=1)
-                    if('html' not in str(r.content)):
-                        mime = magic.Magic(mime=True)
-                        file_type = mime.from_buffer(r.content)
-                        file_extension = f'.{file_type.split("/")[1]}'
-                        if file_extension not in extensions:
-                            raise ValueError()
-                        if file_extension == '.png' and not google_image_seen:
-                            google_image_seen = True
-                            raise ValueError()
-                        file_name = str(keyword_to_search[i]) + "_" + str(j + 1) + file_extension
-                        with open(os.path.join(path, file_name), 'wb') as file:
-                            file.write(r.content)
-                        bar.update(bar.currval + 1)
-                    else:
-                        j -= 1
-                except Exception as e:
-                    j -= 1
-                j += 1
-
+        while i < count:
+            global SCANNER_COUNTER
+            SCANNER_COUNTER = -1
+            url = urls_[i]
+            path = self.generate_dir(search[i])
+            raw_html = self._download_page(url) # Download the entire page from the google Picture search
+            for _ in range(limit+1):
+                webpage_url = scan_webpage(raw_html, self._extensions, timer)
+                if webpage_url:
+                    file_name = Downloader.gen_fn(webpage_url, search[i])
+                    cache_out[file_name] = [path, webpage_url]
+                else:
+                    pass
+                bar.update(bar.currval + 1)
             i += 1
         bar.finish()
+        if verbose:
+            for url in cache_out:
+                print(url)
+        if cache:
+            self._cached_urls = cache_out
+        if not cache_out:
+            print('==='*15 + ' < ' + 'NO PICTURES FOUND' + ' > ' + '==='*15)
+        return cache_out
 
-
-    def _create_directories(self, main_directory, name):
-        name = name.replace(" ", "_")
-        try:
-            if not os.path.exists(main_directory):
-                os.makedirs(main_directory)
-                time.sleep(0.2)
-                path = (name)
-                sub_directory = os.path.join(main_directory, path)
-                if not os.path.exists(sub_directory):
-                    os.makedirs(sub_directory)
+    def download(self, keywords=None, limit=1, verbose=False, cache=True, download_cache=False, timer=None):
+        if not download_cache:
+            content = self.search_urls(keywords, limit, verbose, cache, timer)
+        else:
+            content = self._cached_urls
+            if not content:
+                print('Downloader has not URLs saved in Memory yet, run Downloader.search_urls to find pics first')
+        for name, (path, url) in content.items():
+            with open(os.path.join(path, name), 'wb') as file:
+                file.write(url.content)
+            if verbose:
+                print(f'File Name={name}, Downloaded from {url.url}')
             else:
-                path = (name)
-                sub_directory = os.path.join(main_directory, path)
-                if not os.path.exists(sub_directory):
-                    os.makedirs(sub_directory)
+                pass
 
-        except OSError as e:
-            if e.errno != 17:
-                raise
-            pass
+    def _create_directories(self, name):
+        dir_path = os.path.join(self._directory, name)
+        try:
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+        except OSError:
+            raise
+        self.get_dirs.update([name])
         return
 
-    def _download_page(self,url):
+    def generate_dir(self, dir_name):
+        """Generate Path and Directory, also check if Directory exists or not """
+        dir_name = dir_name.replace(" ", "_")
+        if dir_name in self.get_dirs:
+            pass
+        else:
+            self._create_directories(dir_name)
+        return self._directory + dir_name
 
-        try:
-            headers = {}
-            headers['User-Agent'] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
-            req = urllib.request.Request(url, headers=headers)
-            resp = urllib.request.urlopen(req)
-            respData = str(resp.read())
-            return respData
+    @staticmethod
+    def gen_fn(check, name):
+        """Create a file name string and generate a random identifiers otherwise won't import same pic twice"""
+        id = str(hex(random.randrange(1000)))
+        mime = magic.Magic(mime=True)
+        file_type = mime.from_buffer(check.content)
+        file_extension = f'.{file_type.split("/")[1]}'
+        file_name = str(name) + "_" + id[2:] + file_extension
+        return file_name
 
-        except Exception as e:
-            print(e)
-            exit(0)
+    def flush_cache(self):
+        """Clear the Downloader instance cache"""
+        self._cached_urls = set()
